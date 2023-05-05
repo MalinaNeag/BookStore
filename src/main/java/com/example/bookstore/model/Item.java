3d35c6eb64f4e4d54b5da51d1b6051a1aa01cbf1
Malina002 <100318695+Malina002@users.noreply.github.com>
2023-05-05 19:34:10 +0300

    BS-43 Implement Catalogue Item Addition Functionality

--- src/main/java/com/example/bookstore/model/Item.java
+++ src/main/java/com/example/bookstore/model/Item.java
@@ -1,4 +1,25 @@
 package com.example.bookstore.model;
 
 public class Item {
+    private String name;
+    private String path;
+    private String imgPath;
+
+    public Item(String name, String path, String imgPath) {
+        this.name = name;
+        this.path = path;
+        this.imgPath = imgPath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
 }
